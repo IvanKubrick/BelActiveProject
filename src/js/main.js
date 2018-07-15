66e1b8baa54b2fc771b3dd8454406ef0fa2bdf5c
Ivan Grushevich <githubmail911@gmail.com>
2018-07-15 17:34:30 +0300

    update licenses and about pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,16 @@
 (function () {
-    const certificatesWrapper = document.querySelector('.licenses');
+    const certificatesWrapper = document.querySelector('.licences__wrapper');
     const modal = document.querySelector('.modal');
     const modalImg = document.querySelector('.modal__img');
     const closeBtn = document.querySelector('.modal__close');
 
     certificatesWrapper.addEventListener('click', (event) => {
-        event.preventDefault();
-        if (event.target.tagName !== 'IMG') {
+        console.log(event)
+
+        if (!event.target.classList.contains('licenses__block')) {
             return;
         } else {
-            let img = event.target;
+            let img = event.target.children[0];
             modalImg.src = img.src;
             modalImg.alt = img.alt;
             modal.classList.add('modal--open');
