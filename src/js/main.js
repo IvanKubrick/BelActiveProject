85aa77b0ab436eab3fab27bc95f5a7cec5af0574
Ivan Grushevich <githubmail911@gmail.com>
2019-04-19 12:59:47 +0300

    Update pages, create project list

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,33 @@
-(function () {
-    const certificatesWrapper = document.querySelector('.licences__wrapper');
-    const modal = document.querySelector('.modal');
-    const modalImg = document.querySelector('.modal__img');
-    const closeBtn = document.querySelector('.modal__close');
+; (function () {
+    if (window.innerWidth > 600) {
+        const certificatesWrapper = document.querySelector('.licences__wrapper');
+        const modal = document.querySelector('.modal');
+        const modalImg = document.querySelector('.modal__img');
+        const closeBtn = document.querySelector('.modal__close');
 
-    certificatesWrapper.addEventListener('click', (event) => {
-        if (!event.target.classList.contains('licenses__block')) {
-            return;
-        } else {
-            let img = event.target.children[0];
-            modalImg.src = img.src;
-            modalImg.alt = img.alt;
-            modal.classList.add('modal--open');
-        }
-    });
+        certificatesWrapper.addEventListener('click', (event) => {
+            if (!event.target.classList.contains('licenses__block')) {
+                return;
+            } else {
+                let img = event.target.children[0];
+                modalImg.src = img.src;
+                modalImg.alt = img.alt;
+                modal.classList.add('modal--open');
+            }
+        });
 
-    closeBtn.addEventListener('click', () => modal.classList.remove('modal--open'));
+        closeBtn.addEventListener('click', () => modal.classList.remove('modal--open'));
 
-    document.documentElement.addEventListener('keydown', (event) => {
-        if (modal.classList.contains('modal--open')) {
-            modal.classList.remove('modal--open');
-        }
-    });
+        document.documentElement.addEventListener('keydown', (event) => {
+            if (modal.classList.contains('modal--open')) {
+                modal.classList.remove('modal--open');
+            }
+        });
 
-    modal.addEventListener('click', (event) => {
-        if (event.target === modal && modal.classList.contains('modal--open')) {
-            modal.classList.remove('modal--open');
-        }
-    });
+        modal.addEventListener('click', (event) => {
+            if (event.target === modal && modal.classList.contains('modal--open')) {
+                modal.classList.remove('modal--open');
+            }
+        });
+    }
 })();
